e6b4be07dac0a6f0fc0b2e471fb8d20d2ef29cf0
lependu <lependu@protonmail.com>
2017-10-30 14:18:10 +0100

    Adds marko-starter-babel, lassoCongig

--- project.js
+++ project.js
@@ -1,3 +1,17 @@
-module.exports = require("marko-starter").projectConfig({
-  name: "marko-starter-demo" // Optional, but added here for demo purposes
+const MarkoStarter = require('marko-starter')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+MarkoStarter.plugins = ['marko-starter-babel']
+
+module.exports = MarkoStarter.projectConfig({
+  routePathPrefix: '/',
+  lassoConfig: {
+    bundlingEnabled: isProduction,
+    fingerprintsEnabled: isProduction,
+    minify: isProduction,
+    plugins: [
+      'lasso-marko'
+    ]
+  }
 });
